cd253866dfeeebd4f5b27bf206ea35682b2c6aa5
jwgjanssen <jwg.janssen@gmail.com>
2015-10-09 15:29:00 +0200

    Added reading watersensor

--- test-code/egw_ShowSensorData/egw_ShowSensorData.ino
+++ test-code/egw_ShowSensorData/egw_ShowSensorData.ino
@@ -2,19 +2,25 @@
 
 Port portLeft (1);
 Port portRight (2);
+Port electr_led (1);
 Port portGas (3);
+Port gas_led (3);
+Port portWater (4);
+Port water_led (4);
 
 word valueLeft = 0;
 word valueRight = 0;
 word valueGas = 0;
-
+word valueWater = 0;
 
 void setup() {
     Serial.begin(57600);
-    Serial.println("\n[Monitoring electricity & gas sensors]");
+    Serial.println("\n[Monitoring electricity & gas & water sensors]");
     portLeft.mode(INPUT);
     portRight.mode(INPUT);
     portGas.mode(INPUT);
+    portWater.mode(INPUT);
+    water_led.mode(OUTPUT);
 }
   
 void loop() {
@@ -22,12 +28,15 @@ void loop() {
   valueLeft = portLeft.anaRead();
   valueRight = portRight.anaRead();
   valueGas = portGas.anaRead();
+  valueWater = portWater.anaRead();
   Serial.print("Left value  : ");
   Serial.print(valueLeft);
-  Serial.print("    Right value : ");
+  Serial.print("  Right value : ");
   Serial.print(valueRight);
-  Serial.print("    Gas value : ");
+  Serial.print("  Gas value : ");
   Serial.print(valueGas);
+  Serial.print("  Water value : ");
+  Serial.print(valueWater);
   Serial.println("");
   delay(50);
 }
